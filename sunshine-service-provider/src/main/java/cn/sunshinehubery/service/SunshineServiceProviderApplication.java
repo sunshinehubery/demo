299ab0fd755a85a8362b9eff2af0ca43a283ab2d
furong <1036317945@qq.com>
2019-08-26 14:14:49 +0800

    注册服务到eureka

--- sunshine-service-provider/src/main/java/cn/sunshinehubery/service/SunshineServiceProviderApplication.java
+++ sunshine-service-provider/src/main/java/cn/sunshinehubery/service/SunshineServiceProviderApplication.java
@@ -2,9 +2,11 @@ package cn.sunshinehubery.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("cn.sunshinehubery.service.mapper")
 public class SunshineServiceProviderApplication {
 
